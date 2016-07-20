72b2803efbe61ead4a5565dfb34546b1d30a9a40
litlw <hasaunderritchie@gmail.com>
2016-07-19 20:53:08 -0400

    worked on stuff in class

---
+++ elfiegram/elfiegram/ProfileViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ProfileViewController.swift
+//  elfiegram
+//
+//  Created by Hugh on 2016-07-19.
+//  Copyright © 2016 Hugh. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+
+    @IBOutlet weak var ProfileImageView: UIImage!
+
+    @IBOutlet weak var ProfileNameLabel: UILabel!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ProfileNameLabel.text = "yourName"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cameraButtonPressed(sender: AnyObject) {
+        print("cameraButton Pressed")
+        // 1: Create an ImagePickerController
+        let pickerController = UIImagePickerController()
+        
+        // 2: Self in this line refers to this View Controller
+        //    Setting the Delegate Property means you want to receive a message
+        //    from pickerController when a specific event is triggered.
+        pickerController.delegate = self
+        
+        if TARGET_OS_SIMULATOR == 1 {
+            // 3. We check if we are running on a Simulator
+            //    If so, we pick a photo from the simulator’s Photo Library
+            // We need to do this because the simulator does not have a camera
+            pickerController.sourceType = .PhotoLibrary
+        } else {
+            // 4. We check if we are running on an iPhone or iPad (ie: not a simulator)
+            //    If so, we open up the pickerController's Camera (Front Camera, for selfies!)
+            pickerController.sourceType = .Camera
+            pickerController.cameraDevice = .Front
+            pickerController.cameraCaptureMode = .Photo
+        }
+        
+        // Preset the pickerController on screen
+        self.presentViewController(pickerController, animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
